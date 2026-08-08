ff40c7bce0de6fe850ee7fe8c5b669aaa95e417d
kaviikaviya88-lab <kaviikaviya88@gmail.com>
2026-08-08 13:29:15 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,35 +1,23 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for student_feedback project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-
-    # College Home Page
-    path('', views.index, name='index'),
-
-    # Student Feedback Form
-    path('feedback/', views.home, name='home'),
-
-    # Admin Login
-    path('admin-login/', views.admin_login, name='admin_login'),
-
-    # Admin Dashboard
-    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
-
-    # Mark Feedback as Addressed
-    path(
-        'mark-addressed/<int:feedback_id>/',
-        views.mark_addressed,
-        name='mark_addressed'
-    ),
-
-    # Delete Feedback
-    path(
-        'delete-feedback/<int:feedback_id>/',
-        views.delete_feedback,
-        name='delete_feedback'
-    ),
-
-    # Admin Logout
-    path('admin-logout/', views.admin_logout, name='admin_logout'),
-
+    path('admin/', admin.site.urls),  # Django's built-in admin site
+    path('', include('feedback.urls')),  # Our feedback app URLs
 ]
