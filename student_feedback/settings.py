2648eefae3fe946b4109689c3470658eb53ca210
kaviikaviya88-lab <kaviikaviya88@gmail.com>
2026-08-15 14:44:06 +0530

    Update settings.py

--- student_feedback/settings.py
+++ student_feedback/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'student-feedback-system-8lvr.onrender.com']
 
 # Application definition
 
